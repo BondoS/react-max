5c4373913ff30a10a103b12f75c7830095369871
Abdelrahman Serag <abdo.serag90@gmail.com>
2019-05-11 00:58:48 +0200

    adding HOC

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,48 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import Aux from '../../../hoc/Auxiliary';
+import classes from './Person.module.css';
+import withClass from '../../../hoc/withClass';
+import AuthContext from '../../../context/auth-context'
+
+class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  static contextType = AuthContext;
+
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
+  }
+  render() {
+    console.log('[person.js] rendering... ', this.props)
+    return (
+      <Aux>
+        {this.context.authenticated ? <p>Authenticated</p> : <p>Please Log in</p>}
+        <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          // ref={(inputEl) => {this.inputElement = inputEl}}
+          ref={this.inputElementRef}
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Aux>
+    )
+  }
+};
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
+
+export default withClass(Person, classes.Person);
---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,56 @@
+import React, { PureComponent } from 'react'
+
+import Person from './Person/Person';
+
+class Persons extends PureComponent {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps')
+  //   return state;
+  // }
+
+  // componentWillReceiveProps (props) {
+  //   console.log('[Persons.js] componentWillReceiveProps', props)
+  // }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[Persons.js] shouldComponentUpdate');
+  //   if (
+  //     nextProps.persons !== this.props.persons ||
+  //     nextProps.clicked !== this.props.clicked ||
+  //     nextProps.changed !== this.props.changed
+  //   ) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return null;
+  }
+
+  componentDidUpdate() {
+    console.log('[Persons.js] componentDidUpdate')
+  }
+
+  componentWillUnmount() {
+    console.log('[Persons.js] componentWillUnmount')
+  }
+  render() {
+    console.log('[Persons.js] rendering... ', this.props);
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={event => this.props.changed(event, person.id)}
+          isAuth={this.props.isAuthenticated}
+        />
+      );
+    })
+  }
+}
+
+export default Persons;
